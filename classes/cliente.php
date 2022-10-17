a7e2c936fcd19c0d12e99f17025b38dd2518e518
gustavo1046 <gustavo1046@hotmail.com>
2022-10-17 07:45:58 -0300

    adicionando classe de cliente

---
+++ classes/cliente.php
@@ -0,0 +1,22 @@
+<?php
+class {
+    public string $nome;
+    public int $cpf;
+
+    public function __construct(string $nome, int $cpf){
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getCpf(){
+        return $this->cpf;
+    }
+
+
+}
+
+?>
